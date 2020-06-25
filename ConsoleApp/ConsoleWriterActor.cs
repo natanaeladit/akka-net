f384331b2ee97697b57616eb654207d726eba6fd
natanaeladit <nata.adit@gmail.com>
2020-06-25 14:01:17 +0800

    first commit

---
+++ ConsoleApp/ConsoleWriterActor.cs
@@ -0,0 +1,34 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class ConsoleWriterActor : UntypedActor
+    {
+        protected override void OnReceive(object message)
+        {
+            var msg = message as string;
+
+            // make sure we got a message
+            if (string.IsNullOrEmpty(msg))
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Please provide an input.\n");
+                Console.ResetColor();
+                return;
+            }
+
+            // if message has even # characters, display in red; else, green
+            var even = msg.Length % 2 == 0;
+            var color = even ? ConsoleColor.Red : ConsoleColor.Green;
+            var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
+            Console.ForegroundColor = color;
+            Console.WriteLine(alert);
+            Console.ResetColor();
+        }
+    }
+}
